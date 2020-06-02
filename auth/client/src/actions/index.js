44ad2cda513ea46ea6d6a87f2a54660970688bdb
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-02 16:31:42 +0300

    React app "SignIn" a user & saving his JWT in the Redux Store

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -15,6 +15,19 @@ export const signup = (formProps) => async dispatch => {
 
 };
 
+export const signin = (formProps) => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signin', formProps);
+    dispatch({type: AUTH_USER, payload: response.data.token});
+    localStorage.setItem('token', response.data.token); //persist JWT in localStorage so that on page refresh we dont lose it
+
+    history.push('feature')
+  } catch(e)  {
+    dispatch({type: AUTH_ERROR, payload: 'Invalid login credentials'})
+  }
+
+};
+
 export const signout = () => {
   localStorage.removeItem('token');
 
---
+++ auth/client/src/components/auth/Signin.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {reduxForm, Field} from 'redux-form';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
+import * as actions from '../../actions';
+
+class Signin extends Component {
+  onSubmit = (formProps) => {
+    this.props.signin(formProps);
+  }
+
+  render() {
+
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label>Email</label>
+          <Field name="email" type="text" component="input" autoComplete="none" />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          <Field name="password" type="password" component="input" autoComplete="none" />
+        </fieldset>
+        <div>{this.props.errorMessage}</div>
+        <button>Sign In</button>
+      </form>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {errorMessage: state.auth.errorMessage}
+}
+
+export default compose( //apply multiple HOCs to a single Component
+  connect(mapStateToProps, actions),
+  reduxForm({form: 'signin'})
+)(Signin);
